0b1227cc7aece34fcf8b2883b8eef58f30b30f27
Joshua <joshua.rapior@freshmile.com>
2018-03-26 12:35:29 +0200

    Rollback to 0.0.2

--- src/CounterHelperTrait.php
+++ src/CounterHelperTrait.php
@@ -3,6 +3,7 @@
 namespace FriendsOfCake\TestUtilities;
 
 /**
+ *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
@@ -11,23 +12,23 @@ namespace FriendsOfCake\TestUtilities;
 trait CounterHelperTrait
 {
 
-    /**
-     * List of counters used by this test case
-     *
-     * @var array
-     */
+/**
+ * List of counters used by this test case
+ *
+ * @var array
+ */
     protected $_expectationCounters = [];
 
-    /**
-     * Returns a matcher that matches when the method it is evaluated for
-     * is invoked _next_
-     *
-     * Permits using multiple named counters
-     *
-     * @param  mixed $name string or object
-     * @return \PHPUnit_Framework_MockObject_Matcher_InvokedAtIndex
-     */
-    public function nextCounter($name = '')
+/**
+ * Returns a matcher that matches when the method it is evaluated for
+ * is invoked _next_
+ *
+ * Permits using multiple named counters
+ *
+ * @param mixed $name string or object
+ * @return \PHPUnit_Framework_MockObject_Matcher_InvokedAtIndex
+ */
+    public function next($name = '')
     {
         if (is_object($name)) {
             $name = get_class($name);
@@ -37,7 +38,6 @@ trait CounterHelperTrait
         } else {
             $this->_expectationCounters[$name] += 1;
         }
-
         return $this->at($this->_expectationCounters[$name]);
     }
 }
